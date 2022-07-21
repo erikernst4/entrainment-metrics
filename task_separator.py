f1becccd31a51d1769b2772fc3ccb8275a135485
Erik <erik.ernst@muttdata.ai>
2022-07-21 19:03:53 -0300

    feat: read wavs

--- task_separator.py
+++ task_separator.py
@@ -9,21 +9,32 @@ arg_parser = argparse.ArgumentParser(description="Cut wav files for each task an
 arg_parser.add_argument("-s", "--session-folder", type=str, help="Folder with wavs, tasks and words")
 arg_parser.add_argument("-o", "--output-path", help="Directory to save the output files")
 
+def read_wavs(path):
+    wav_A = (None, None)
+    wav_B = (None, None)
+    for wav_file in glob.glob(os.path.join(path, "*.wav")):
+        file_extensions = wav_file.split(".")
+
+        samplerate, data = wavfile.read(wav_file)
+        wav = (samplerate, data)
+
+        if "A" in file_extensions:
+            wav_A = wav
+        elif "B" in file_extensions:
+            wav_B = wav
+                
+    return wav_A, wav_A 
+
 def read_words(path):
     words_A = []
     words_B = []
-    #for file in os.listdir(directory):
     for filename in glob.glob(os.path.join(path, "*.words")):
-        #file_extensions = os.path.split(file)[1]
         file_extensions = filename.split(".")
         with open(filename, encoding="utf-8", mode="r") as word_file:
-            #lines = word_file.readlines()
             lines = word_file.read().splitlines() 
 
-            #if filename.endswith("A.words"):
             if "A" in file_extensions:
                 words_A = lines
-            #elif filename.endswith("B.words"):
             elif "B" in file_extensions:
                 words_B = lines
                 
@@ -39,9 +50,10 @@ def main() -> None:
 
     words_A, words_B = read_words(session_dir)
 
-    print(words_A)
-    print(words_B)
+    wav_A, wav_B = read_wavs(session_dir)
 
+    print(wav_A)
+    print(wav_B)
 
 if __name__ == "__main__":
     main()
