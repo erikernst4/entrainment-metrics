7f2021ee7e02e69d095cd0634bb1e7ba24bf6552
Erik <erik.ernst@muttdata.ai>
2022-08-21 13:38:49 -0300

    feat: add test cases empty and silence

--- test_tama.py
+++ test_tama.py
@@ -11,6 +11,16 @@ from tama import get_frames, get_interpausal_units
 class TAMATestCase(TestCase):
     def setUp(self):
         self.cases = {
+            'empty': {
+                'audio': wavfile.read("./data/empty.wav"),
+                'expected_ipus': [],
+                'expected_frames': [],
+            },
+            'silence': {
+                'audio': wavfile.read("./data/silence.wav"),
+                'expected_ipus': [],
+                'expected_frames': [MissingFrame(0.0, 0.3999375)],
+            },
             'small': {
                 'audio': wavfile.read("./data/200-300-100.wav"),
                 'expected_ipus': [
@@ -30,8 +40,20 @@ class TAMATestCase(TestCase):
                         ],
                     )
                 ],
-            }
+            },
         }
+
+    def test_interpausal_units_separation_empty(self):
+        self.assertEqual(
+            get_interpausal_units("./data/empty.words"),
+            self.cases['empty']['expected_ipus'],
+        )
+
+    def test_interpausal_units_separation_silence(self):
+        self.assertEqual(
+            get_interpausal_units("./data/silence.words"),
+            self.cases['silence']['expected_ipus'],
+        )
 
     def test_interpausal_units_separation_small(self):
         expected = self.cases['small']
@@ -42,9 +64,25 @@ class TAMATestCase(TestCase):
         )
         self.assertEqual(expected_ipus, result_ipus)
 
-    def test_frame_separation_one_frame(self):
+    def test_frame_separation_empty(self):
+        expected_frames = self.cases['empty']['expected_frames']
+        result_frames: List[Union[Frame, MissingFrame]] = get_frames(
+            wav_fname="./data/empty.wav", words_fname="./data/empty.words"
+        )
+        self.assertEqual(expected_frames, result_frames)
+
+    def test_frame_separation_silence(self):
+        expected_frames = self.cases['silence']['expected_frames']
+        result_frames: List[Union[Frame, MissingFrame]] = get_frames(
+            wav_fname="./data/silence.wav", words_fname="./data/silence.words"
+        )
+        print(expected_frames[0])
+        print(result_frames[0])
+        self.assertEqual(expected_frames, result_frames)
+
+    def test_frame_separation_small(self):
         expected_frames = self.cases['small']['expected_frames']
         result_frames: List[Union[Frame, MissingFrame]] = get_frames(
-            "", "./data/200-300-100.wav", "./data/200-300-100.words"
+            wav_fname="./data/200-300-100.wav", words_fname="./data/200-300-100.words"
         )
         self.assertEqual(expected_frames, result_frames)
