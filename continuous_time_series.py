c26b5e013de2d15898dbbb14c2c87ce270e58895
Erik <erik.ernst@muttdata.ai>
2022-10-04 19:45:58 -0300

    feat: remove outliers from ipus_feature_values

--- continuous_time_series.py
+++ continuous_time_series.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 import numpy as np
 from sklearn.neighbors import KNeighborsRegressor
@@ -28,7 +28,8 @@ class TimeSeries:
         feature: str,
         interpausal_units: List[InterPausalUnit],
         method: str,
-        k: int = None,
+        k: Optional[int] = None,
+        MAX_DEVIATIONS: Optional[int] = None,
         **kwargs,
     ) -> None:
         self.ipus: List[InterPausalUnit] = interpausal_units
@@ -44,6 +45,10 @@ class TimeSeries:
             X = self._get_middle_points_in_time()
             X = X.reshape(-1, 1)
             y = self._get_interpausal_units_feature_values(feature)
+
+            # Remove outliers from feature values
+            y = self._remove_outliers_from_ipus_feature_values(y, MAX_DEVIATIONS)
+
             self.model.fit(X, y)
         else:
             # Here is some space to build your own model!
@@ -63,6 +68,28 @@ class TimeSeries:
         Returns a list with the feature value for each IPU.
         """
         return np.array([ipu.feature_value(feature) for ipu in self.ipus])
+
+    def _remove_outliers_from_ipus_feature_values(
+        self,
+        ipus_feature_values: np.ndarray,
+        MAX_DEVIATIONS: Optional[int] = None,
+    ) -> np.ndarray:
+        """
+        Remove outliers from the ipus feature values
+
+
+        Outliers are values with a distance from the mean greater than MAX_DEVIATIONS times the standard deviation
+        """
+        if MAX_DEVIATIONS is None:
+            MAX_DEVIATIONS = 3
+
+        mean: np.float64 = np.mean(ipus_feature_values)
+        standard_deviation: np.float64 = np.std(ipus_feature_values)
+        distance_from_mean: np.ndarray = abs(ipus_feature_values - mean)
+        not_outlier: np.ndarray = (
+            distance_from_mean < MAX_DEVIATIONS * standard_deviation
+        )
+        return ipus_feature_values[not_outlier]
 
     def _get_middle_points_in_time(
         self,
