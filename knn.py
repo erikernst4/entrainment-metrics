fix: convergence calculation

knn.py
@@ -1,6 +1,6 @@
 import argparse
 from pathlib import Path
-from typing import List, Optional, Tuple
+from typing import Any, List, Optional, Tuple
 
 import numpy as np
 
@@ -68,10 +68,12 @@ def calculate_proximity(
 
 
 def calculate_convergence(
-    time_series_a: List[float],
-    time_series_b: List[float],
-) -> float:
-    return np.corrcoef(time_series_a, time_series_b)  # type: ignore
+    time_series_a: np.ndarray,
+    time_series_b: np.ndarray,
+    time_values: np.ndarray,
+) -> np.ndarray:
+    d_t = np.abs(time_series_a - time_series_b) * -1
+    return np.corrcoef(d_t, time_values)
 
 
 def calculate_metric(
@@ -82,7 +84,7 @@ def calculate_metric(
     end: float,
     granularity: Optional[float] = None,
     samplerate: Optional[float] = None,
-) -> float:
+) -> Any:
     """
     Calculate entrainment metrics given a times series from each speaker
 
@@ -94,16 +96,17 @@ def calculate_metric(
     if samplerate is None:
         samplerate = 16000
 
-    values_to_predict = np.arange(start, end, granularity)
-    values_to_predict = values_to_predict.reshape(-1, 1)
+    values_to_predict_in_s = np.arange(start, end, granularity)
+    values_to_predict = values_to_predict_in_s.reshape(-1, 1)
 
     values_a = time_series_a.predict(values_to_predict)
     values_b = time_series_b.predict(values_to_predict)
 
+    res: Any = None
     if metric == "proximity":
         res = calculate_proximity(values_a, values_b)
-    elif metric == "pearson":
-        res = calculate_convergence(values_a, values_b)
+    elif metric == "pearson" or metric == "convergence":
+        res = calculate_convergence(values_a, values_b, values_to_predict_in_s)
     else:
         raise ValueError("Not a valid metric")
     return res
@@ -159,7 +162,7 @@ def main() -> None:
 
     common_start, common_end = calculate_common_support(time_series_a, time_series_b)
     print(f"Common support: {(common_start, common_end)}")
-    metric_result: float = calculate_metric(
+    metric_result: Any = calculate_metric(
         args.metric, time_series_a, time_series_b, common_start, common_end
     )
     print(f"{args.metric}: {metric_result}")