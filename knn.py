57554e6c0fe8732b891fdd051dae1d81307fa778
Erik <erik.ernst@muttdata.ai>
2022-10-13 19:08:27 -0300

    refactor+fix: separate script from code + truncate values

--- knn.py
+++ knn.py
@@ -1,52 +1,8 @@
-import argparse
-from pathlib import Path
-from typing import Any, List, Optional, Tuple
+from typing import Optional, Tuple
 
 import numpy as np
 
 from continuous_time_series import TimeSeries
-from interpausal_unit import InterPausalUnit
-from utils import get_interpausal_units, print_audio_description
-
-arg_parser = argparse.ArgumentParser(
-    description="Return a times series for a speaker for a task"
-)
-arg_parser.add_argument(
-    "-a", "--audio-file-a", type=str, help="Audio .wav file for a speaker A"
-)
-arg_parser.add_argument(
-    "-b", "--audio-file-b", type=str, help="Audio .wav file for a speaker B"
-)
-arg_parser.add_argument(
-    "-wa", "--words-file-a", type=str, help=".words file for a speaker A"
-)
-arg_parser.add_argument(
-    "-wb", "--words-file-b", type=str, help=".words file for a speaker B"
-)
-arg_parser.add_argument(
-    "-f", "--feature", type=str, help="Feature to calculate time series"
-)
-arg_parser.add_argument(
-    "-ga", "--pitch-gender-a", type=str, help="Gender of the pitch of speaker A"
-)
-arg_parser.add_argument(
-    "-gb", "--pitch-gender-b", type=str, help="Gender of the pitch of speaker B"
-)
-arg_parser.add_argument(
-    "-k",
-    "--k-neighboors",
-    type=str,
-    help="Amount of neighboors to approximate with knn",
-)
-arg_parser.add_argument(
-    "-e", "--extractor", type=str, help="Extractor to use for calculating IPUs features"
-)
-arg_parser.add_argument(
-    "-m",
-    "--metric",
-    type=str,
-    help="Entrainment metric from a/p evolution functions to calculate",
-)
 
 
 def calculate_common_support(
@@ -58,6 +14,18 @@ def calculate_common_support(
     return common_start, common_end
 
 
+def truncate_values(
+    values: np.ndarray,
+    start: float,
+    end: float,
+) -> None:
+    greater_than_end_values = values > end
+    values[greater_than_end_values] = end
+
+    less_than_start_values = values < start
+    values[less_than_start_values] = start
+
+
 def calculate_proximity(
     time_series_a: TimeSeries,
     time_series_b: TimeSeries,
@@ -66,6 +34,7 @@ def calculate_proximity(
     granularity: float,
 ) -> float:
     values_to_predict_in_s = np.arange(start, end + granularity, granularity)
+    truncate_values(values_to_predict_in_s, start, end)
     values_to_predict = values_to_predict_in_s.reshape(-1, 1)
 
     time_series_values_a = time_series_a.predict(values_to_predict)
@@ -86,6 +55,7 @@ def calculate_convergence(
 ) -> float:
 
     values_to_predict_in_s = np.arange(start, end + granularity, granularity)
+    truncate_values(values_to_predict_in_s, start, end)
     values_to_predict = values_to_predict_in_s.reshape(-1, 1)
 
     time_series_values_a = time_series_a.predict(values_to_predict)
@@ -111,9 +81,12 @@ def calculate_synchrony(
         values_to_predict_b_in_s = np.arange(
             start, end - synchrony_delta + granularity, granularity
         )
-        values_to_predict_a, values_to_predict_b = values_to_predict_a_in_s.reshape(
-            -1, 1
-        ), values_to_predict_b_in_s.reshape(-1, 1)
+        truncate_values(values_to_predict_a_in_s, start, end)
+        truncate_values(values_to_predict_b_in_s, start, end)
+        values_to_predict_a, values_to_predict_b = (
+            values_to_predict_a_in_s.reshape(-1, 1),
+            values_to_predict_b_in_s.reshape(-1, 1),
+        )
 
         time_series_values_a = time_series_a.predict(values_to_predict_a)
         time_series_values_b = time_series_b.predict(values_to_predict_b)
@@ -142,7 +115,7 @@ def calculate_metric(
     if granularity is None:
         granularity = 0.01
 
-    res: Any = None
+    res = None
     metric = metric.lower()
     if metric == "proximity":
         res = calculate_proximity(time_series_a, time_series_b, start, end, granularity)
@@ -155,67 +128,3 @@ def calculate_metric(
     else:
         raise ValueError("Not a valid metric")
     return res
-
-
-def main() -> None:
-    args = arg_parser.parse_args()
-
-    k: int = int(args.k_neighboors)
-    wav_a_fname: Path = Path(args.audio_file_a)
-    words_a_fname: Path = Path(args.words_file_a)
-    ipus_a: List[InterPausalUnit] = get_interpausal_units(words_a_fname)
-    print(f"Amount of IPUs of speaker A: {len(ipus_a)}")
-    print_audio_description("A", wav_a_fname)
-
-    wav_b_fname: Path = Path(args.audio_file_b)
-    words_b_fname: Path = Path(args.words_file_b)
-    ipus_b: List[InterPausalUnit] = get_interpausal_units(words_b_fname)
-    print(f"Amount of IPUs of speaker B: {len(ipus_b)}")
-    print_audio_description("B", wav_b_fname)
-
-    for ipu in ipus_a:
-        ipu.calculate_features(
-            audio_file=wav_a_fname,
-            pitch_gender=args.pitch_gender_a,
-            extractor=args.extractor,
-        )
-
-    for ipu in ipus_b:
-        ipu.calculate_features(
-            audio_file=wav_b_fname,
-            pitch_gender=args.pitch_gender_b,
-            extractor=args.extractor,
-        )
-
-    time_series_a: List[float] = TimeSeries(
-        interpausal_units=ipus_a,
-        feature=args.feature,
-        method='knn',
-        k=k,
-    )
-    print("----------------------------------------")
-    print(f"Time series of A: {time_series_a}")
-
-    time_series_b: List[float] = TimeSeries(
-        interpausal_units=ipus_b,
-        feature=args.feature,
-        method='knn',
-        k=k,
-    )
-    print(f"Time series of B: {time_series_b}")
-    print("----------------------------------------")
-
-    common_start, common_end = calculate_common_support(time_series_a, time_series_b)
-    print(f"Common support: {(common_start, common_end)}")
-    metric_result: Any = calculate_metric(
-        args.metric,
-        time_series_a,
-        time_series_b,
-        common_start,
-        common_end,
-    )
-    print(f"{args.metric}: {metric_result}")
-
-
-if __name__ == "__main__":
-    main()
---
+++ script_knn.py
@@ -0,0 +1,108 @@
+import argparse
+from pathlib import Path
+from typing import List
+
+from continuous_time_series import TimeSeries
+from interpausal_unit import InterPausalUnit
+from knn import calculate_common_support, calculate_metric
+from utils import get_interpausal_units, print_audio_description
+
+arg_parser = argparse.ArgumentParser(
+    description="Return a times series for a speaker for a task"
+)
+arg_parser.add_argument(
+    "-a", "--audio-file-a", type=str, help="Audio .wav file for a speaker A"
+)
+arg_parser.add_argument(
+    "-b", "--audio-file-b", type=str, help="Audio .wav file for a speaker B"
+)
+arg_parser.add_argument(
+    "-wa", "--words-file-a", type=str, help=".words file for a speaker A"
+)
+arg_parser.add_argument(
+    "-wb", "--words-file-b", type=str, help=".words file for a speaker B"
+)
+arg_parser.add_argument(
+    "-f", "--feature", type=str, help="Feature to calculate time series"
+)
+arg_parser.add_argument(
+    "-ga", "--pitch-gender-a", type=str, help="Gender of the pitch of speaker A"
+)
+arg_parser.add_argument(
+    "-gb", "--pitch-gender-b", type=str, help="Gender of the pitch of speaker B"
+)
+arg_parser.add_argument(
+    "-k",
+    "--k-neighboors",
+    type=str,
+    help="Amount of neighboors to approximate with knn",
+)
+arg_parser.add_argument(
+    "-e", "--extractor", type=str, help="Extractor to use for calculating IPUs features"
+)
+arg_parser.add_argument(
+    "-m",
+    "--metric",
+    type=str,
+    help="Entrainment metric from a/p evolution functions to calculate",
+)
+
+
+def main() -> None:
+    args = arg_parser.parse_args()
+
+    k: int = int(args.k_neighboors)
+    wav_a_fname: Path = Path(args.audio_file_a)
+    words_a_fname: Path = Path(args.words_file_a)
+    ipus_a: List[InterPausalUnit] = get_interpausal_units(words_a_fname)
+    print(f"Amount of IPUs of speaker A: {len(ipus_a)}")
+    print_audio_description("A", wav_a_fname)
+
+    wav_b_fname: Path = Path(args.audio_file_b)
+    words_b_fname: Path = Path(args.words_file_b)
+    ipus_b: List[InterPausalUnit] = get_interpausal_units(words_b_fname)
+    print(f"Amount of IPUs of speaker B: {len(ipus_b)}")
+    print_audio_description("B", wav_b_fname)
+
+    for ipu in ipus_a:
+        ipu.calculate_features(
+            audio_file=wav_a_fname,
+            pitch_gender=args.pitch_gender_a,
+            extractor=args.extractor,
+        )
+
+    for ipu in ipus_b:
+        ipu.calculate_features(
+            audio_file=wav_b_fname,
+            pitch_gender=args.pitch_gender_b,
+            extractor=args.extractor,
+        )
+
+    time_series_a: List[float] = TimeSeries(
+        interpausal_units=ipus_a,
+        feature=args.feature,
+        method='knn',
+        k=k,
+    )
+
+    time_series_b: List[float] = TimeSeries(
+        interpausal_units=ipus_b,
+        feature=args.feature,
+        method='knn',
+        k=k,
+    )
+
+    common_start, common_end = calculate_common_support(time_series_a, time_series_b)
+    print(f"Common support: {(common_start, common_end)}")
+    metric_result: float = calculate_metric(
+        args.metric,
+        time_series_a,
+        time_series_b,
+        common_start,
+        common_end,
+    )
+    print(f"{args.metric}: {metric_result}")
+
+
+if __name__ == "__main__":
+    main()
