191827dc2c3b84d8e59616d16bc41251ff34eca0
Erik <erik.ernst@muttdata.ai>
2023-01-18 17:06:12 -0300

    feat: add name to each time series in plot

--- speechalyze/continuous/utils.py
+++ speechalyze/continuous/utils.py
@@ -11,15 +11,34 @@ def plot_time_series(
     plot_ipus: Optional[bool] = None,
     granularity: Optional[float] = None,
     legend: Optional[bool] = None,
+    time_series_a_name: Optional[str] = None,
+    time_series_b_name: Optional[str] = None,
     **kwargs
 ):
     if legend is None:
         legend = True
+
+    if time_series_a_name is None:
+        time_series_a_name = "time_series_a"
+
+    if time_series_b_name is None:
+        time_series_b_name = "time_series_b"
+
     time_series_a.plot(
-        plot_ipus, granularity, color="C0", show=False, label="time_series_a", **kwargs
+        plot_ipus,
+        granularity,
+        color="C0",
+        show=False,
+        label=time_series_a_name,
+        **kwargs
     )
     time_series_b.plot(
-        plot_ipus, granularity, color="C1", show=False, label="time_series_b", **kwargs
+        plot_ipus,
+        granularity,
+        color="C1",
+        show=False,
+        label=time_series_b_name,
+        **kwargs
     )
     if legend:
         handles, labels = plt.gca().get_legend_handles_labels()
