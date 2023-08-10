282336531bf3852ea9f9294ec088c439731c96be
Erik <erik.ernst@muttdata.ai>
2023-08-10 16:59:19 -0300

    fix: mock numba import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage','sphinx.ext.napoleon']
 templates_path = ['_templates']
 exclude_patterns = []
 
-autodoc_mock_imports = ["audiofile", "opensmile", "sklearn", "scipy", "parselmouth"]
+autodoc_mock_imports = ["audiofile", "opensmile", "sklearn", "scipy", "parselmouth", "numba"]
 
 
 
